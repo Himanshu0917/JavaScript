8126a1f9ad4e8613a210e2956f83789025ea4433
Himanshu0917 <132185307+Himanshu0917@users.noreply.github.com>
2023-11-29 06:59:33 +0000

    Date and Time

---
+++ 01_basics/06_datesinJs.js
@@ -0,0 +1,11 @@
+let myDate  = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toTimeString())
+console.log(myDate.toDateString())
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp/1000)
+
+
