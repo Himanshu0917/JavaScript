1cdce066855cf24d18eda6077a401a8082d58e21
Himanshu0917 <132185307+Himanshu0917@users.noreply.github.com>
2023-12-07 16:22:29 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// IFFY
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION
+
+(function code(){
+    // Named IIFE
+    console.log("Hello coding");
+})();
+
+
+
+(() => {
+    console.log(`Database connectes`);
+})() 
